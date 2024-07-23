3457bbe84bfd8d38da12f5f620c5773b6ccc52fb
Jeremy Moon <jeremy.moon16@gmail.com>
2024-07-23 10:29:36 -0500

    refactor source code to be able to run in python scripts
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 A collection of statistical models to predict the outcomes of individual games in baseball, soccer, and basketball.
 
-Please read the Notebook files for more information about the project!
 
 ### Clone repository
 
 Navigate to your desired directory on the terminal, then clone the project repository:
 
 ```bash
-$ git clone https://github.com/hyojunm/game-prediction-models.git
+git clone https://github.com/hyojunm/game-prediction-models.git
 ```
 
 ### Install dependencies
@@ -22,52 +21,41 @@ Ensure that you have the following dependencies installed:
 To run some visualizations, `graphviz` may need to be installed with:
 
 ```bash
-$ sudo apt install graphviz
+sudo apt install graphviz
 ```
 
 ### Set up environment
 
-1. Create a virtual environment
+1. Create a virtual environment (replace `my_env` with a different name if you'd like)
 
 ```bash
-$ python3 -m venv [env_name]
+python3 -m venv my_env
 ```
 
 2. Activate the virtual environment
 
 ```bash
-$ source [env_name]/bin/activate
+source my_env/bin/activate
 ```
 
-3. Install ipykernel
+3. Install Python packages
 
 ```bash
-$ python3 -m pip install ipykernel
+python3 -m pip install pandas scikit-learn matplotlib numpy graphviz statsmodels pybaseball ipykernel
 ```
 
-4. Create a Jupyter kernel
+4. Convert Jupyter Notebook files to Python scripts (replace `random_forest_regression.ipynb` with the Notebook file you'd like to see)
 
 ```bash
-$ python3 -m ipykernel install --user --name=[env_name]
+jupyter nbconvert --to script random_forest_regression.ipynb
 ```
 
 ### Run the project
 
-Start up Jupyter Lab (or Notebook) with either command:
+Run the converted Python script with (replace `random_forest_regression.py` with the appropriate file name, if different):
 
 ```bash
-$ jupyter lab
-$ jupyter notebook
+python3 random_forest_regression.py
 ```
 
-**Note:** Necessary data files are already included within the repository, so there is no need to run the `*_data_collection.ipynb` files again (unless you would like to edit the data).
-
-### Troubleshooting
-
-* Make sure you are running your Notebook on the correct virtual environment kernel. Go to **Kernel > Change Kernel...** to verify you are using the correct kernel and/or change it to the correct one. The name should match the `[env_name]` you passed in when creating the kernel.
-
-    ![Choose the correct kernel](kernel.png)
-
-* Double check that you have installed all Python packages necessary to run the project. At the top of the Notebook file, run the appropriate cells to install and import the packages.
-
-    ![Install and import all necessary packages](install.png)
+**Note:** Necessary data files are already included within the repository, so there is no need to run the `baseball_data_collection.ipynb` file again (unless you would like to edit the data).
---
+++ analysis.py
@@ -0,0 +1,137 @@
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, Normalizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, make_scorer
+from sklearn.linear_model import LinearRegression, LogisticRegression, PoissonRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+import graphviz
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
+# figure out which features (columns) are being considered most by the model
+def get_feature_importances(cols, model):
+    feature_importances = {
+        'feature': [],
+        'weight': []
+    }
+    
+    for i in range(len(cols)):
+        feature_importances['feature'].append(cols[i])
+        feature_importances['weight'].append(f'{round(model.feature_importances_[i] * 100, 2)}%')
+    
+    return pd.DataFrame(feature_importances)
+
+
+# visualizing decision trees
+# https://www.datacamp.com/tutorial/random-forests-classifier-python
+def visualize_decision_trees(model, cols, n_trees=1, max_depth=2):
+    for i in range(n_trees):
+        tree = model.estimators_[i]
+        dot_data = export_graphviz(tree,
+                                   feature_names=cols,  
+                                   filled=True,  
+                                   max_depth=max_depth, 
+                                   impurity=False, 
+                                   proportion=True)
+        graph = graphviz.Source(dot_data)
+        display(graph)
+
+
+# get variance inflation factors for each feature - testing for multicollinearity
+# https://www.datasklr.com/ols-least-squares-regression/multicollinearity
+def get_vif(x):
+    x_temp = sm.add_constant(x)
+
+    vif = pd.DataFrame()
+    vif['feature'] = x_temp.columns
+    vif['variance_inflation_factor'] = [variance_inflation_factor(x_temp.values, i) for i in range(x_temp.values.shape[1])]
+    
+    return vif
+
+
+# given a results dataframe, calculate the accuracy (in %) of the model in terms of predicting wins/losses
+def wl_accuracy(results):
+    num_correct = 0
+    num_incorrect = 0
+    
+    for i in range(len(results)):
+        game = results.iloc[i]
+    
+        pred_result = 1 if game['home_pred'] > game['away_pred'] else 0 # where 1 = home team wins
+        actual_result = 1 if game['home_true'] > game['away_true'] else 0
+        
+        if pred_result == actual_result:
+            num_correct += 1
+        else:
+            num_incorrect += 1
+
+    return (num_correct / (num_correct + num_incorrect)) * 100
+
+
+# calculate season records based on predictions
+def season_record(df, results):
+    team_records = {}
+
+    for i in range(len(results)):
+        away_team = df['away_team'].iloc[13047 + i]
+        home_team = df['home_team'].iloc[13047 + i]
+        home_team_won = results['away_pred'].iloc[i] <= results['home_pred'].iloc[i]
+    
+        if away_team not in team_records:
+            team_records[away_team] = 0
+    
+        if home_team not in team_records:
+            team_records[home_team] = 0
+    
+        team_records[away_team] += 1 if not home_team_won else 0
+        team_records[home_team] += 1 if home_team_won else 0
+
+    print('Season records:')
+    count = 0
+    
+    for team in sorted(team_records, key=lambda x: team_records[x], reverse=True):
+        count += 1
+        print(f'{team}: {team_records[team]}-{162 - team_records[team]}', end='\t')
+        
+        if count % 5 == 0:
+            print()
+
+
+# calculate each team's runs scored per game based on predictions
+def runs_per_game(df, results):
+    runs_scored = {}
+
+    for i in range(len(results)):
+        away_team = df['away_team'].iloc[13047 + i]
+        home_team = df['home_team'].iloc[13047 + i]
+        
+        if away_team not in runs_scored:
+            runs_scored[away_team] = 0
+    
+        if home_team not in runs_scored:
+            runs_scored[home_team] = 0
+
+        runs_scored[away_team] += results['away_pred'].iloc[i]
+        runs_scored[home_team] += results['home_pred'].iloc[i]
+
+    print('Runs scored per game:')
+    count = 0
+    
+    for team in sorted(runs_scored, key=lambda x: runs_scored[x], reverse=True):
+        count += 1
+        print(f'{team}: {round(runs_scored[team] / 162, 2)} ({round(runs_scored[team])})', end='\t')
+        
+        if count % 5 == 0:
+            print()
