a93a44705234d94f40625dfa1e908a69dd1d8443
Jeremy Moon <jeremy.moon16@gmail.com>
2024-07-26 09:44:25 -0500

    add helper python file for splitting data by season
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

---
+++ data_format.py
@@ -0,0 +1,53 @@
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, Normalizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, make_scorer
+from sklearn.linear_model import LinearRegression, LogisticRegression, PoissonRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+import graphviz
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+from analysis import get_feature_importances, visualize_decision_trees, get_vif, wl_accuracy, season_record, runs_per_game
+
+
+# using game_data_v6.csv
+season_indices = {
+    2015: (0, 0),
+    2016: (0, 0),
+    2017: (0, 2430),
+    2018: (2430, 4861),
+    2019: (4861, 7290),
+    2020: (7290, 8188),
+    2021: (8188, 10617),
+    2022: (10617, 13047),
+    2023: (13047, 15477),
+    2024: (0, 0)
+}
+
+
+# create the appropriate training and testing data based on home/away, dropping columns as needed
+def create_data(df, drop_cols=['away_score', 'home_score', 'away_team', 'home_team'],
+                y_col='away_score', split_by='random', season=2023):
+    y = df[y_col]
+    x = df.drop(drop_cols, axis=1)
+
+    if split_by == 'season':
+        season_start, season_end = season_indices[season]
+        x_train, x_test = x.iloc[0:season_start] + x.iloc[season_end:], x.iloc[season_start:season-end]
+        y_train, y_test = x.iloc[0:season_start] + x.iloc[season_end:], x.iloc[season_start:season-end]
+    else:
+        x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True)
+    
+    return x_train, x_test, y_train, y_test
